94282eb39740380e24575b5949bf12fbb1cada24
k <kulsumk2005@gmail.com>
2025-11-28 09:39:30 -0500

    testing

--- dailyPlanner/src/dailyPlanner/Start.java
+++ dailyPlanner/src/dailyPlanner/Start.java
@@ -1,11 +1,14 @@
 package dailyPlanner;
 
+//im adding stuff
+
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Gui gui1 = new Gui();
 		gui1.firstScreen();
+
 	}
 
 }
